47aea4a07a169eae632c9c926e769858275a8343
kglanos <130902847+kglanos@users.noreply.github.com>
2023-09-14 22:50:33 +0200

    gallery update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainerElement = document.querySelector(".gallery");
+const imagesMarkup = createItemsMarkup(galleryItems);
+galleryContainerElement.insertAdjacentHTML("beforeend", imagesMarkup);
+
+function createItemsMarkup(item) {
+    return galleryItems
+    .map(({ preview, original, description }) => {
+        return `
+        <li class="gallery__link">
+            <a class="gallery__item" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"          
+            alt="${description}"
+        />
+        </a>
+        </li>
+    </ul>`;
+    })
+    .join("");
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+    captionType: "alt",
+});
