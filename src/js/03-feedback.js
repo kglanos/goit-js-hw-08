b8107027efc217ee41d8f105894553d9a05c4e73
kglanos <130902847+kglanos@users.noreply.github.com>
2023-09-16 00:39:35 +0200

    Task 3 update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+    document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector(".feedback-form");
+    const emailInput = form.querySelector('input[name="email"]');
+    const messageTextarea = form.querySelector('textarea[name="message"]');
+
+    const saveToLocalStorageThrottled = throttle(() => {
+    const formData = {
+        email: emailInput.value,
+        message: messageTextarea.value,
+    };
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+    }, 500);
+
+    const loadFromLocalStorage = () => {
+    const savedFormData = localStorage.getItem("feedback-form-state");
+    if (savedFormData) {
+        const parsedData = JSON.parse(savedFormData);
+        emailInput.value = parsedData.email;
+        messageTextarea.value = parsedData.message;
+    }
+};
+
+    emailInput.addEventListener("input", saveToLocalStorageThrottled);
+    messageTextarea.addEventListener("input", saveToLocalStorageThrottled);
+
+    loadFromLocalStorage();
+
+    form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    
+    const formData = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+    };
+    console.log("", formData);
+
+    localStorage.removeItem("feedback-form-state");
+    emailInput.value = "";
+    messageTextarea.value = "";
+    });
+});
